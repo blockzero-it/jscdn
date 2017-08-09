4057a0f33fe40be269306f1b33d5ef821b751429
blockzero-it <30805510+blockzero-it@users.noreply.github.com>
2017-08-09 13:53:34 +0200

    Create CryptoPrice.js

---
+++ CryptoPrice.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+  $.each($('coincap'), function() {
+    var thisCoincap = $(this);
+    try {
+      var currency = thisCoincap.attr('currency').toLowerCase();
+      var FIAT = thisCoincap.attr('fiat').toLowerCase();
+      var APIv1 = 'https://api.coinmarketcap.com/v1/ticker/' + currency + '/?convert=' + FIAT;
+
+      $.getJSON(APIv1, function(json) {
+        if (json['error']) {
+          thisCoincap.html('CoinNotFound');
+          return;
+        }
+
+        var data = json[0]['price_' + FIAT];
+        var price = 'CurrencyNotFound';
+
+        if (data) {
+          price = parseFloat(data).toFixed(2);
+        }
+
+        thisCoincap.html(price);
+      }).fail(function() {
+        thisCoincap.html('CoinNotFound');
+      });
+    } catch (err) {
+      thisCoincap.html('CoinCapError');
+    }
+  });
+});
