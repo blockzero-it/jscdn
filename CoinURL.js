3edb059b369561832cb07fe78ff4afc050c02037
blockzero-it <30805510+blockzero-it@users.noreply.github.com>
2017-08-09 17:28:11 +0200

    Update CoinURL.js

--- CoinURL.js
+++ CoinURL.js
@@ -1,31 +1,31 @@
 jQuery(document).ready(function($) {
   var exclude = Array("blockzero.it", "twitter.com", "facebook.com", "creativecommons.org", "iubenda.com");
-
   var id = "c8e2fbb5a3d46cbf59c8a42ef25eabf6";
-  var redirect = "http://cur.lv/redirect.php?id=" + id + "&url=";
 
+  //CODE
+  var redirect = "http://cur.lv/redirect.php?id=" + id + "&url=";
   var links = $("a[href^='http']");
 
   $.each(links, function() {
     var link = $(this).attr('href');
-    var nocoin = $(this).data('nocoin');
+    var nocoin = $(this).is('[nocoin]');
 
     if (nocoin) {
-      return true;
+      return;
     }
 
     var deny = false;
-    $.each(exclude, function() {
-      if (link.indexOf($(this))) {
+    $.each(exclude, function(n, val) {
+      if (link.indexOf(val) != -1) {
         deny = true;
-        return true;
+        return;
       }
     });
 
     if (deny) {
-      return true;
+      return;
     }
 
-    link.attr('href', redirect + encodeURIComponent(link));
+    $(this).attr('href', redirect + encodeURIComponent(link));
   });
 });
