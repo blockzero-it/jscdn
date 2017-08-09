aa3429a80e8e765d742dc4582e4a01b588b6623a
blockzero-it <30805510+blockzero-it@users.noreply.github.com>
2017-08-09 17:10:00 +0200

    Update CoinURL.js
    Completely re-coded

--- CoinURL.js
+++ CoinURL.js
@@ -1,42 +1,31 @@
-jQuery(document).ready(function($){
-    var include = Array();
-    var exclude = Array("blockzero.it", "twitter.com", "facebook.com", "creativecommons.org", "iubenda.com");
-    var id = "c8e2fbb5a3d46cbf59c8a42ef25eabf6";
-    var redirect = "http://cur.lv/redirect.php?id=" + id + "&url=";
-    var links = $("a[href^='http']");
-    
-    for(var i = 0; i < links.length; i++) {
-        var url = $(links[i]).attr("href");
-        
-        var nocoin = $(links[i]).attr("nocoin");
-        if(nocoin){
-            continue;
-        }
-        
-        var deny = false;
-        for(var j = 0; j < exclude.length; j++) {
-            if(url.indexOf(exclude[j]) != -1) {
-                deny = true;
-                break;
-            }
-        }
-        if(deny) {
-            continue;
-        }
-        
-        if(include.length > 0) {
-            var allow = false;
-            for(var j = 0; j < include.length; j++) {
-                if(url.indexOf(include[j]) != -1) {
-                    allow = true;
-                    break;
-                }
-            }
-            if(!allow) {
-                continue;
-            }
-        }
-        
-        $(links[i]).attr("href",  redirect + encodeURIComponent(url));
+jQuery(document).ready(function($) {
+  var exclude = Array("blockzero.it", "twitter.com", "facebook.com", "creativecommons.org", "iubenda.com");
+
+  var id = "c8e2fbb5a3d46cbf59c8a42ef25eabf6";
+  var redirect = "http://cur.lv/redirect.php?id=" + id + "&url=";
+
+  var links = $("a[href^='http']");
+
+  $.each(links, function() {
+    var link = $(this).attr('href');
+    var nocoin = $(this).data('nocoin');
+
+    if (nocoin) {
+      continue;
     }
+
+    var deny = false;
+    $.each(exclude, function() {
+      if (link.indexOf($(this))) {
+        deny = true;
+        return;
+      }
+    });
+
+    if (deny) {
+      continue;
+    }
+
+    link.attr('href', redirect + encodeURIComponent(link));
+  });
 });
