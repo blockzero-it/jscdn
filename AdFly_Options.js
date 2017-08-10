6d871e3e954212e7ac801a77026cba5af068e60a
blockzero-it <30805510+blockzero-it@users.noreply.github.com>
2017-08-10 12:04:43 +0200

    Create AdFly_Options.js

---
+++ AdFly_Options.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+  var adfly_id = 17642537; 
+  var adfly_advert = 'int'; 
+  var popunder = true; 
+  var exclude_domains = ['blockzero.it', 'facebook.com', 'twitter.com'];
+  $.$.ajaxSetup({ cache: true });
+  $.getScript('https://cdn.ay.gy/js/link-converter.js', function(a, b, c) {});
+});
