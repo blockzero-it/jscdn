79e025baf48dac89ea0c556475bf6016e9a1f26c
blockzero-it <30805510+blockzero-it@users.noreply.github.com>
2017-08-10 12:22:06 +0200

    Update AdFly_Options.js

--- AdFly_Options.js
+++ AdFly_Options.js
@@ -1,5 +1,6 @@
 var adfly_id = 17642537;
 var adfly_advert = 'int';
-var popunder = false;
+var popunder = true;
+var adfly_nofollow = true;
 var adfly_domain = 'out.blockzero.it';
 var exclude_domains = ['blockzero.it', 'facebook.com', 'twitter.com', 'creativecommons.org'];
